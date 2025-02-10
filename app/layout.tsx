ab0fcfc5b30247e8fc1134d33f458d56783477d1
Your Name <you@example.com>
2025-02-10 19:51:13 +0530

    add theme transition effects to layout and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} transition-colors duration-1000`}>
+			<body className={`${inter.className} theme-transition`}>
 				<SessionProvider>{children}</SessionProvider>
 			</body>
 		</html>
