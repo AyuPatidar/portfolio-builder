26643b5d3689c8c380dfe3e4ac32d5334edb692d
AyuPatidar <ayushpatidar755@gmail.com>
2025-02-17 15:37:06 +0530

    make education section responsive

--- components/sections/Education.tsx
+++ components/sections/Education.tsx
@@ -2,7 +2,6 @@ import { education } from "@/constants";
 import React from "react";
 import { motion } from "framer-motion";
 import { GraduationCap, Landmark } from "lucide-react";
-import { Card } from "../ui/card";
 
 const Education = () => {
 	return (
@@ -13,7 +12,7 @@ const Education = () => {
 			<h2 className="text-3xl font-bold mb-12 text-center">
 				<span className="text-primary">Education</span>
 			</h2>
-			<ol className={"flex flex-col relative w-full mx-auto py-8 gap-4"}>
+			<ol className={"flex flex-col relative w-full mx-auto gap-4"}>
 				{education.map((ed, index) => (
 					<motion.div
 						key={index}
@@ -25,13 +24,13 @@ const Education = () => {
 							ease: "easeOut",
 						}}
 					>
-						<li className={"relative w-full mb-8 last:mb-0"}>
-							<div className="grid grid-cols-[auto_auto_1fr] gap-4 items-start">
+						<li className={"relative mb-8 last:mb-0"}>
+							<div className="grid grid-cols-[auto_auto_1fr] md:grid-cols-[1fr_auto_1fr] gap-4 items-start">
 								{/* Period */}
-								<div className="flex justify-end items-center pt-2">
+								<div className="flex justify-end items-center pt-3">
 									<p
 										className={
-											"text-sm font-medium tracking-tight text-secondary-foreground"
+											"text-xs md:text-sm font-medium tracking-tight text-secondary-foreground"
 										}
 									>
 										{ed.period}
@@ -43,11 +42,13 @@ const Education = () => {
 									<div className="relative z-10">
 										<div
 											className={
-												"relative flex items-center justify-center rounded-full ring-8 ring-background shadow-sm h-12 w-12 bg-primary text-primary-foreground"
+												"relative flex items-center justify-center rounded-full ring-8 ring-background shadow-sm h-10 w-10 md:h-12 md:w-12 bg-primary text-primary-foreground"
 											}
 										>
 											<div
-												className={"flex items-center justify-center h-6 w-6"}
+												className={
+													"flex items-center justify-center h-5 w-5 md:h-6 md:w-6"
+												}
 											>
 												{ed.college ? <GraduationCap /> : <Landmark />}
 											</div>
@@ -59,33 +60,29 @@ const Education = () => {
 								</div>
 
 								{/* Card */}
-								<Card>
-									<div className={"flex flex-col gap-2"}>
-										<div className={"flex flex-col"}>
-											<h3
-												className={
-													"font-semibold text-lg leading-none text-secondary-foreground"
-												}
-											>
-												{ed.school || ed.college}
-											</h3>
-											<p>{ed.degree || ed.grade}</p>
-										</div>
-										<div>
-											<p className="text-sm">CGPA: {ed.cgpa}</p>
-											<p className="text-sm">
-												{ed.coursework && "Coursework: "}
-												{ed.coursework &&
-													ed.coursework.map((cw, index) => (
-														<span key={index}>
-															{index != 0 && ", "}
-															{cw}
-														</span>
-													))}
-											</p>
-										</div>
+								<div className={"md:w-1/2 flex flex-col gap-2 pt-3 md:pt-2"}>
+									<div className={"flex flex-col"}>
+										<h3 className="font-semibold text-base md:text-lg leading-none text-secondary-foreground">
+											{ed.school || ed.college}
+										</h3>
+										<p className="text-sm md:text-base">
+											{ed.degree || ed.grade}
+										</p>
 									</div>
-								</Card>
+									<div>
+										<p className="text-xs md:text-sm">CGPA: {ed.cgpa}</p>
+										<p className="text-xs md:text-sm text-wrap">
+											{ed.coursework && "Coursework: "}
+											{ed.coursework &&
+												ed.coursework.map((cw, index) => (
+													<span key={index}>
+														{index != 0 && ", "}
+														{cw}
+													</span>
+												))}
+										</p>
+									</div>
+								</div>
 							</div>
 						</li>
 					</motion.div>
