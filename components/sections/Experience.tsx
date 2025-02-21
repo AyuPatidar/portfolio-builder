b9510e559436fee10e9d93341ab72f104c4aa111
AyuPatidar <ayushpatidar755@gmail.com>
2025-02-21 20:14:51 +0530

    refine experience code

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { experience } from "@/constants";
 import {
 	Card,
@@ -9,8 +9,25 @@ import {
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Badge } from "../ui/badge";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
 const Experience = () => {
+	const [windowWidth, setWindowWidth] = useState<number>(0);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		handleResize();
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<section
 			id="experience"
@@ -20,25 +37,28 @@ const Experience = () => {
 				Industry <span className="text-primary">Experience</span>
 			</h2>
 			<Tabs defaultValue={"0"}>
-				<TabsList className="h-full flex justify-around items-center w-full overflow-x-scroll scrollbar-none">
-					{experience.map((exp, index) => (
-						<TabsTrigger
-							value={index.toString()}
-							key={index}
-							className={`px-2 md:px-8 text-sm md:text-base font-bold data-[state=active]:text-primary`}
-						>
-							{window.innerWidth > 768 && exp.jobDesignation + " @ "}
-							{exp.company}
-						</TabsTrigger>
-					))}
-				</TabsList>
+				<ScrollArea className="w-full bg-accent rounded-t-lg">
+					<TabsList className="h-full p-2 flex justify-around items-center">
+						{experience.map((exp, index) => (
+							<TabsTrigger
+								value={index.toString()}
+								key={index}
+								className={`px-2 md:px-8 text-sm md:text-base font-bold data-[state=active]:text-primary`}
+							>
+								{windowWidth > 768 && exp.jobDesignation + " @ "}
+								{exp.company}
+							</TabsTrigger>
+						))}
+					</TabsList>
+					<ScrollBar orientation="horizontal" />
+				</ScrollArea>
 				{experience.map((exp, index) => (
 					<TabsContent
 						value={index.toString()}
 						key={index}
 					>
-						<Card className="bg-card hover:shadow-xl transition-shadow duration-300">
-							<CardHeader className="space-y-0">
+						<Card className="bg-card rounded-none rounded-b-lg hover:shadow-xl transition-shadow duration-300">
+							<CardHeader className="rounded-none space-y-0">
 								<CardTitle className="text-lg text-primary font-extrabold">
 									{exp.jobDesignation} @ {exp.company}
 								</CardTitle>
